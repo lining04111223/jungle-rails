a33b715cf92377fad856f2f50bebf49eaad1af8a
Ning Li <lining04111223@gmail.com>
2022-11-18 06:34:18 +0000

    dashboard-counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
   def show
+    @num_products = Product.count
+    @num_product_items = Product.sum('quantity')
+    @num_categories = Category.count
+
   end
 end
