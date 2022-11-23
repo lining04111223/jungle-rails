9960a3789a86a57bc07b38fe48c47283f6a05224
Ning Li <lining04111223@gmail.com>
2022-11-23 03:58:41 +0000

    feature/testing-product-page

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,9 @@
+describe("product_details.spec.js", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("should bring you to the product details page", () => {
+    cy.get("article").first().click();
+    cy.url().should("include", "/products/2");
+  });
+});
